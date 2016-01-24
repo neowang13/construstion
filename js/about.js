7ef249a9e76a2fcb92ba3d1d9f0122a96497d0b8
neowang13 <neowang13@gmail.com>
2016-01-24 14:55:13 -0800

    slide changed

--- js/about.js
+++ js/about.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
 	$('.bxslider').bxSlider({
 	  adaptiveHeight: true,
 	  mode: 'fade',
-	  height: '200'
+	  height: '200',
+	  // auto: true,
+	  // pause: 1000,
 	});
 
 
